f6028c07c66b87c9ad3e8e81534f8276402b8ef7
Farhad-ali GOULAMABASSE <fgoulama@e3r6p16.clusters.42paris.fr>
2023-05-17 21:06:07 +0200

    first commit

--- main.c
+++ main.c
@@ -31,45 +31,64 @@ t_heap	*create_new_heap() {
 	heap->prev = NULL;
 	heap->block_count = 0;
 	heap->free_size = sz;
-	heap->all_size = sz;
+	heap->total_size = sz;
+	return heap;
 }
 
 t_block	*create_new_block(t_heap *heap, size_t size) {
-	t_block block;
+	t_block *block = (t_block*)HEAP_SHIFT(heap);
+	if (heap->block_count > 0) {
+		int offset = 0;
+		t_block	*cur = block;
+		for (int i = 0; i < heap->block_count; i++) {
+			cur = (t_block *)(HEAP_SHIFT(heap) + offset);
+			offset += cur->size + sizeof(t_block);
+		}
+		block = (t_block *)(HEAP_SHIFT(heap) + offset);
+		cur->next = block;
+	}
 	block->next = NULL;
 	block->prev = NULL;
 	block->size = size;
 	block->freed = false;
 	heap->block_count += 1;
 	heap->free_size -= size;
-	return block;
+	return BLOCK_SHIFT(block);
 }
 
 void        *my_malloc(size_t size) {
-	t_heap	*heap = HEAD;
 	if (HEAD == NULL) {
-		heap = create_new_heap();
+		HEAD = create_new_heap();
+		printf("fewf");
 	}
 
-	t_block	*start = HEAP_SHIFT(heap);
-	if (heap->block_count == 0) {
-		start = create_new_block(size);
-		return BLOCK_SHIFT(start);
-	} else {
-		
+	t_heap	*heap = HEAD;
+	t_block *ret = create_new_block(heap, size);
+	printf("----heap----\n");
+	printf("free size: %d\n", heap->free_size);
+	printf("block count: %d\n", heap->block_count);
+	printf("----blocks list----\n");
+	t_block *cur = (t_block *)HEAP_SHIFT(heap);
+	while (cur) {
+		printf("size: %d\n", cur->size);
+		cur = cur->next;
 	}
-
-
+	return ret;
 }
 
 int main() {
-    // int *array = my_malloc(sizeof(int) * 10);
+    int *array = my_malloc(sizeof(int) * 5);
     // array[0] = 3;
     // array[9] =  8;
     // for (int i = 0; i < 10; i++) {
     //     printf("%d\n", array[i]);
     // }
-	int sz = getpagesize();
-	printf("%d\n",sz);
+	int *array2 = my_malloc(sizeof(int) * 10);
+	int *array3 = my_malloc(sizeof(int) * 15);
+    array2[0] = 3;
+    array2[9] =  8;
+    // for (int i = 0; i < 10; i++) {
+    //     printf("%d\n", array2[i]);
+    // }
     // free(array);
 }
